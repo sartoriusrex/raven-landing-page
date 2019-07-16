ba4bf5d9c9cf540344750ffb4331431ecfb71f30
sartoriusrex <dennis.q.mai@gmail.com>
2019-07-15 14:03:47 -1000

    finish making footer section responsive and add back to app

--- src/components/App.js
+++ src/components/App.js
@@ -18,7 +18,7 @@ const App = () => {
         <ProjectSection />
         <OpinionSection />
         <ContactSection />
-        {/* <FooterSection /> */}
+        <FooterSection />
       </>
     )
 }
